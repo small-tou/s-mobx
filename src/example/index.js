28e67ee5902ea650dc26dc6d09aef2038faf520e
yutou <sunxinyu@souche.com>
2017-04-07 10:30:56 +0800

    hello

---
+++ src/example/index.js
@@ -0,0 +1,18 @@
+import {
+    BaseModel,
+    distance,
+    price
+} from './../index';
+
+class IndexItem extends BaseModel{
+
+    icon = '';
+    @price('wy')
+    price = 10;
+    @distance('wkm')
+    mileage = 10
+}
+
+var item = new IndexItem();
+
+console.log(JSON.stringify(item))
---
+++ src/index.js
@@ -0,0 +1,10 @@
+
+import BaseModel from './BaseModel';
+import distance from './distance';
+import price from './price';
+
+export {
+  BaseModel,
+  distance,
+  price
+}
