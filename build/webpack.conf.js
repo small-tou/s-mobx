134b6a2741bfb6fa3b58c354337286c95042e058
chuangker <chuangker@foxmail.com>
2017-04-21 13:42:23 +0800

    chore: add webpack config

---
+++ build/webpack.conf.js
@@ -0,0 +1,39 @@
+const webpack = require('webpack');
+const path = require('path');
+
+module.exports = {
+    entry: {
+        main: './src/s-mobx.js'
+    },
+    output: {
+        path: path.resolve(__dirname, '../dist'),
+        filename: 's-mobx.min.js'
+    },
+    resolve: {
+        extensions: ['.js']
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                loader: 'babel-loader',
+                exclude: /node_modules/,
+                options: {
+                    presets: [ 'es2015' ],
+                    plugins: [
+                        'transform-decorators-legacy',
+                        'transform-class-properties',
+                    ],
+                    babelrc: false
+                }
+            }
+        ]
+    },
+    plugins: [
+        new webpack.optimize.UglifyJsPlugin({
+            compress: {
+                warnings: false
+            }
+        })
+    ]
+};
